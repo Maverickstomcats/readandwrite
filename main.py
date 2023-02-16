f0b14dbf33396ca4ec05a927ef6e3f83e4394b0e
tunatuna tunatuna <113930372+Maverickstomcats@users.noreply.github.com>
2023-02-16 18:40:35 +0000

    did things :D

--- main.py
+++ main.py
@@ -1,2 +1,24 @@
-# remember to fork this and push to github
+# # remember to fork this and push to github
 
+# f = open('test.txt','a')
+# f.write("ridge racer pls come back namco pls")
+# #read gives us everything
+# #readline gives us the first line (supposedly)
+# #print(f.writelines())
+# f.close()
+
+# f = open("employees.txt",'a')
+# f.write("Jesus - assistant manager")
+# f.close()
+
+# with open("test.txt","w") as f:
+#  # f_contents = f.read()
+#   f.write('NAMCO WHERE IS MY RIDGE RACER 8, BRING IT, COWARDS.')
+# print(f)
+
+# with open('open.txt','r') as f:
+#   for line in f:
+#     print(line, end='')
+
+with open('period7.txt','w') as f:
+  f.write("maverick where are my chips :(")
